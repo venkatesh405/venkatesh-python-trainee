3f7c3a9b8f576ff22bad0c9796b8917776f6497d
venkatesh405 <58566543+venkatesh405@users.noreply.github.com>
2020-02-28 22:31:57 +0530

    Add files via upload

---
+++ area of traiangle.py
@@ -0,0 +1,6 @@
+a = 2
+b = 4
+c = 8
+s = a + b + c / 2
+area = s*((s-a)*(s-b)*(s-c))
+print('the area of traiangle is %0.2f'%area)
---
+++ sum.py
@@ -0,0 +1,4 @@
+num1 = 23
+num2 = 78
+sum = float(num1) + float(num2)
+print('sum of the numbers {0}and {1} is {2}'.format(num1,num2,sum))
